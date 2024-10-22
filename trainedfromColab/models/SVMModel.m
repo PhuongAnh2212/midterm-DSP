15dce5bee751b8e4154971f6a78b7c4a87a32b94
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-10-23 02:57:59 +0700

    done adjust intercept vector

--- trainedfromColab/models/SVMModel.m
+++ trainedfromColab/models/SVMModel.m
@@ -8,7 +8,7 @@ classdef SVMModel
     methods
         function obj = SVMModel(support_vectors, intercept, dual_coefficients)
             obj.SupportVectors = support_vectors;
-            
+
             % Ensure intercept is a row vector
             if iscolumn(intercept)
                 intercept = intercept';  % Convert to row vector
@@ -19,39 +19,26 @@ classdef SVMModel
                 error('Intercept must be a row vector of size 1 x 6.');
             end
             
-            obj.Intercept = intercept;
+            obj.Intercept = intercept;  % Assign the intercept
             obj.DualCoefficients = dual_coefficients;
-            disp('Intercept Size:');
-            disp(size(intercept));
         end
         
         function label = predict(obj, x)
             if iscolumn(x)
                 x = x';  % Transpose to make sure it's a row vector
             end
-        
+            
             % Check if the feature size matches the support vector size
             if size(x, 2) ~= size(obj.SupportVectors, 2)
                 error('Feature size does not match the number of support vectors features.');
             end
-        
+            
             % Calculate decision value for each class
-            decisionValue = (x * obj.SupportVectors');  % Resulting size is 1 x 11502
-            decisionValue = decisionValue * obj.DualCoefficients';  % Resulting size is 1 x 6
-        
-            % Check if obj.Intercept is a column vector and transpose if needed
-            if iscolumn(obj.Intercept)
-                obj.Intercept = obj.Intercept';  % Make it a row vector
-            end
-        
-            % Ensure the intercept is of size 1 x 6
-            if size(obj.Intercept, 2) ~= 6
-                error('Intercept size must be 1 x 6 to match decision values.');
-            end
-        
-            % Add intercept for each class
-            decisionValue = decisionValue + obj.Intercept;  % This should now work
-        
+            decisionValue = (x * obj.SupportVectors') * obj.DualCoefficients;  % Decision values for all classes
+            
+            % Add intercept for each class (broadcasting)
+            decisionValue = decisionValue + obj.Intercept;  % Add intercept (should be a vector)
+            
             % Get the predicted class based on the max decision value
             [~, predictedIndex] = max(decisionValue, [], 2);  % Take the index of the max value along the row
         end
--- trainedfromColab/setup.m
+++ trainedfromColab/setup.m
@@ -4,7 +4,28 @@ function setup()
     dual_coefficients = readmatrix('models/dual_coefficients.csv');
 
     global svmModel;
+    % Display the size of the intercept
+    disp('Size of Intercept:');
+    disp(size(intercept));  % Should output [21, 1]
+    
+    % Check if intercept is too long
+    if size(intercept, 1) >= 6
+        intercept = intercept(1:6, :);  % Take the first 6 values if more than needed
+    else
+        error('Intercept does not have enough values for the classes.');
+    end
+    
+    % Reshape to be a row vector
+    intercept = intercept';  % This makes it a row vector of size [1, 6]
+    
+    % Check the size of the intercept after reshaping
+    disp('Size of Intercept after adjustment:');
+    disp(size(intercept));  % Should output [1, 6]
+
+    
+    % Create the SVM model
     svmModel = SVMModel(support_vectors, intercept, dual_coefficients);
+
 
     disp("Model load successfully")
     
